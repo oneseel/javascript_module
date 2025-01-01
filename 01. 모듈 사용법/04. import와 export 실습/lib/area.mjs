41e10464ba8683827302720e58bc9a3649d273e7
oneseel <seno10065@gmail.com>
2025-01-01 23:20:04 +0900

    01-04. import와 export 실습

---
+++ 01. 모듈 사용법/04. import와 export 실습/lib/area.mjs
@@ -0,0 +1,10 @@
+// 여기에 코드를 작성하세요.
+import { PI } from "./calculator.mjs";
+
+export function circle(x) {
+  return PI * x * x;
+}
+
+export function square(x) {
+  return x * x;
+}
---
+++ 01. 모듈 사용법/04. import와 export 실습/main.mjs
@@ -0,0 +1,6 @@
+// 여기에 코드를 작성하세요.
+import { circle, square } from "./lib/area.mjs";
+
+// 테스트 코드
+console.log(circle(1));
+console.log(square(2));
